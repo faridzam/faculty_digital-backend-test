91a594026570a0cb1149a21278390e77b652e2f5
faridzam <skinel123@gmail.com>
2024-07-02 09:16:36 +0700

    fix: env configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config({path: `.env.${process.env.NODE_ENV}` || `.env`});
+dotenv.config({path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : `.env`});
 
 export default {
   secret: process.env.JWT_SECRET || 'your-secret-key',
